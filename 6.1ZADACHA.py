99db6316e4e531bbb8ce2912918b96337b7cf431
Oleg <sagdeev.ol@yandex.ru>
2024-06-30 21:59:42 +0500

    Add files via upload

---
+++ 6.1ZADACHA.py
@@ -0,0 +1,55 @@
+class Plant:
+    edible = False
+
+    def __init__(self, name):
+        self.name = name
+
+
+class Flower(Plant):
+    pass
+
+
+class Fruit(Plant):
+    edible = True
+
+
+class Animal:
+    alive = True
+    fed = False
+
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self, food):
+        self.food = Plant
+        if Plant.edible == True:
+            Animal.fed = True
+            print(f'{self.name} съел {food.name}')
+        if Plant.edible == False:
+            Animal.alive = False
+            print(f'{self.name} не стал есть {food.name}')
+
+
+class Mammal(Animal):
+    pass
+
+
+class Predator(Animal):
+    pass
+
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(f'{a1.name} жив: {a1.alive}')
+print(f'{a2.name} сытый: {a2.fed}')
+a1.eat(p1)
+a2.eat(p2)
+print(f'{a1.name} жив: {a1.alive}')
+print(f'{a2.name} сытый: {a2.fed}')
+
